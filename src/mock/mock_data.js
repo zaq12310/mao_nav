733dbea7bc6ef4b209468d80110a3fac7ecc13c2
zaq12310 <92127845+zaq12310@users.noreply.github.com>
2026-01-01 02:20:04 +0800

    chore: 更新导航数据 - 2026/1/1 02:20:03

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -447,7 +447,7 @@ export const mockData = {
           "id": "site-1767168991644",
           "name": "elysliver公益站",
           "url": "https://elysiver.h-e.top/",
-          "description": "无法添加到插件中，插件无法正确读取数据，直接用这个",
+          "description": "无法添加到插件中，插件无法正确读取数据\n\n作者：\nhttps://linux.do/u/bytebender/summary",
           "icon": "https://lsky.useforall.com/pixel/2025/12/22/6949494a9f7d9.png"
         },
         {
@@ -468,28 +468,28 @@ export const mockData = {
           "id": "site-1767202937391",
           "name": "Wind Hub",
           "url": "https://api.224442.xyz/login",
-          "description": "无法注册登录，linuxdo等级不够",
+          "description": "无法注册登录，linuxdo等级不够\n\n作者：\nhttps://linux.do/u/beizhi/summary",
           "icon": "https://www.faviconextractor.com/favicon/api.224442.xyz"
         },
         {
           "id": "site-1767203009457",
           "name": "我爱996",
           "url": "https://529961.com/login",
-          "description": "无法注册",
+          "description": "无法注册\n\n作者：\nhttps://linux.do/u/piles/summary",
           "icon": "https://www.faviconextractor.com/favicon/529961.com"
         },
         {
           "id": "site-1767203235199",
           "name": "薄荷api",
           "url": "https://x666.me/login",
-          "description": "无法注册？似乎注册过？",
+          "description": "无法注册？似乎注册过？\n\n作者：\nhttps://linux.do/u/bohe/summary",
           "icon": "https://i.111666.best/image/UQ3YrIrF59JZfaEFGJabrr.png"
         },
         {
           "id": "site-1767203393814",
           "name": "B4U",
           "url": "https://b4u.qzz.io/register",
-          "description": "无法注册",
+          "description": "无法注册\n\n作者：\nhttps://linux.do/u/dabuliu/summary",
           "icon": "https://www.faviconextractor.com/favicon/b4u.qzz.io"
         },
         {
@@ -503,7 +503,7 @@ export const mockData = {
           "id": "site-1767203751080",
           "name": "cone",
           "url": "https://zone.veloera.org",
-          "description": "正常登录，加入插件无法正确读取",
+          "description": "正常登录，加入插件无法正确读取\n\n作者：\nhttps://linux.do/u/neuroplexus/summary",
           "icon": "https://www.faviconextractor.com/favicon/zone.veloera.org"
         }
       ]
